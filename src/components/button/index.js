82d13fe07d99dd6eee8ac6c9cbb82f34332a8e4a
Aaron-24DucAnh24 <90702045+Aaron-24DucAnh24@users.noreply.github.com>
2023-06-24 10:48:57 +0700

    load files

--- src/components/button/index.js
+++
@@ -1,9 +0,0 @@
-import style from './style.module.scss'
-
-export default function HomeButton({onclick, content}) {
-    return (
-        <div className={style.container} onClick={()=>onclick()}>
-            <p className={style.text}>{content}</p>
-        </div>
-    )
-}
--- src/components/pageHeader/index.js
+++
@@ -1,9 +0,0 @@
-import style from './style.module.scss'
-
-export default function PageHeader ({title, icon}) {
-    return (
-        <div className={style.container}>
-            <p className='pageTitle'><span>{icon}</span>{title}</p>
-        </div>
-    )
-}
